536a5b11d7c87f6075d1e9d89babb92d77b257f6
Carlos Oliveira <carlos.oliveira@touchhealth.com.br>
2017-11-02 14:00:00 -0300

    Commit 5
    Change-Id: I3814b0d0f07787568b3da5381332e72271780116

--- Arquivo1.java
+++ Arquivo1.java
@@ -2,7 +2,7 @@ public class Arquivo1 {
 
 	public static void main(String args[]) {
 		// do something
-		throw new RuntimeException("Mensagem de erro 4444444.");
+		throw new RuntimeException("Mensagem de erro 5555555.");
 		// do something
 	}
 
